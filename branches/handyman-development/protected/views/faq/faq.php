bf42275cc16f0d98fc9e192975fe34a273fbb135
Chikaysci <Chikaysci@039cb88d-4c00-4ffa-8d7f-56469ec047c3>
2014-06-05 01:06:35 +0000

    added faq

---
+++ branches/handyman-development/protected/views/faq/faq.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/questions.css">
+<div class="container">
+			<div class="row-fluid margTop15 margBot20">
+				<div class="col-md-12">	
+					<div class="panel panel-danger containFAQ">
+					  <div class="panel-heading"><h3 class="faq-title"><span class="glyphicon glyphicon-question-sign"></span>&nbsp;Frequently Asked Questions</h3></div>
+					  <div class="panel-body">
+						 <div class="faq-intro">
+							<p>Below is a list of our frequently asked questions related to handyman services.  If you don't find the question or answer you're looking for, please feel free to <a href="<?php echo Yii::app()->request->baseUrl; ?>/home/contactus" class="faq-contact"><b>Contact Us.</b></a></p>
+						 </div>
+						 <div class="ask-con">
+							<p class="asks"><span class="glyphicon glyphicon-hand-right"></span><span class="ask-title">Do I need to purchase the materials on my own?</span></p>
+							<p class="answers">You can, but it is not necessary.  All of our technicians have access to local hardware stores and home improvement centers.  We can pick up, deliver and install any materials you desire for the project.</p>
+						 </div>
+						 <div class="ask-con">
+							<p class="asks"><span class="glyphicon glyphicon-hand-right"></span><span class="ask-title">Do you charge by the hour or the job?</span></p>
+							<p class="answers">We do NOT charge by the hour.  We give an estimate for a complete project.  By not charging hourly, we remove the risk of the project taking longer and ultimately costing the customer more than the estimate.</p>
+						 </div>
+						 <div class="ask-con">
+							<p class="asks"><span class="glyphicon glyphicon-hand-right"></span><span class="ask-title">How do I pay for services?</span></p>
+							<p class="answers">You can pay by check, credit card or by a debit card.</p>
+						 </div>
+						 <div class="ask-con">
+							<p class="asks"><span class="glyphicon glyphicon-hand-right"></span><span class="ask-title">Do you work weekends and evenings?</span></p>
+							<p class="answers">Yes!  When you book your appointment with the call center, the call service representative will make every effort to accommodate your schedule.</p>
+						 </div>
+						 <div class="ask-con">
+							<p class="asks"><span class="glyphicon glyphicon-hand-right"></span><span class="ask-title">Can I choose the material colors and manufacturers?</span></p>
+							<p class="answers">Yes!  We leave the choices to you!  You pick the paint colors or the material color.  You can even choose the manufacturer if you desire.  You can also leave the selection of the manufacturer to us.  We only use high quality manufacturers.</p>
+						 </div>
+						 <div class="ask-con">
+							<p class="asks"><span class="glyphicon glyphicon-hand-right"></span><span class="ask-title">Can I trust the handyman in my home or office?</span></p>
+							<p class="answers">All of our technicians go through a thorough screening process.  Our Tier 1 technicians have a minimum of 10 years experience and their own hand-tools.  We know that trust is a major factor when you have someone coming into your home or business, so we make certain all of our technicians are prompt, clean, and professional!</p>
+						 </div>
+					 </div>
+					</div>
+				</div>
+			</div>
+		</div>
